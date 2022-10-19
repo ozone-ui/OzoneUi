ea25e3fce0ecd746f1008d689fe468b2c1c83aa9
abusiraj84 <abusiraj84@gmail.com>
2022-10-19 23:38:54 +0300

    feat(add verients to btn): added other type of buttons

--- packages/core/src/App.vue
+++ packages/core/src/App.vue
@@ -1,51 +1,103 @@
 <template>
   <div>
     <main class="flex flex-col gap-y-3 justify-center items-center h-screen">
-      <Btn
-        text="حجز جلسة"
-        bg="#F4DB3A"
-        color="#000"
-        startIcon="line-md:uploading-loop"
-        endIcon="line-md:sunny-outline-twotone-loop"
-        iconColor="#070000"
-        iconWidth="20"
-      />
-      <Btn
-        text="تسجيل دخول"
-        bg="#3A7BF4"
-        color="#fff"
-        endIcon="mi:chevron-right"
-        iconColor="#fff"
-        iconWidth="20"
-      />
-      <Btn
-        text="رفع ملف"
-        bg="#3AEBF4"
-        color="#000"
-        startIcon="mi:cloud-upload"
-        iconColor="#070000"
-        iconWidth="20"
-      />
-      <Btn
-        text="حذف"
-        bg="#F43A40"
-        color="#fff"
-        startIcon="ant-design:delete-twotone"
-        iconColor="#fff"
-        iconWidth="18"
-      />
+      <transition-group name="list">
+        <Btn
+          key="btn1"
+          text="حجز جلسة"
+          bg="#F4DB3A"
+          color="#000"
+          startIcon="line-md:uploading-loop"
+          endIcon="line-md:sunny-outline-twotone-loop"
+          iconColor="#070000"
+          iconWidth="20"
+        />
+        <Btn
+          key="btn2"
+          text="تسجيل دخول"
+          bg="#3A7BF4"
+          color="#fff"
+          endIcon="mi:chevron-right"
+          iconColor="#fff"
+          iconWidth="20"
+        />
+        <Btn
+          key="btn3"
+          text="رفع ملف"
+          bg="#3AEBF4"
+          color="#000"
+          startIcon="mi:cloud-upload"
+          iconColor="#070000"
+          iconWidth="20"
+        />
+        <Btn
+          key="btn4"
+          text="حذف"
+          bg="#F43A40"
+          color="#fff"
+          startIcon="ant-design:delete-twotone"
+          iconColor="#fff"
+          iconWidth="18"
+        />
+        <Btn
+          key="btn5"
+          border
+          text="طلب المساعدة"
+          bg="#7E3AF4"
+          color="#fff"
+          endIcon="clarity:help-info-line"
+          iconColor="#fff"
+          iconWidth="18"
+          @click="toogleModal()"
+        />
+      </transition-group>
+      <transition name="fade">
+        <div
+          class="w-[600px] h-[200px] rounded-[15px] bg-[#f2f2f2]"
+          v-if="showModal"
+        ></div>
+      </transition>
     </main>
   </div>
 </template>
 
-<script lang="ts">
-import { Btn } from "./components/btn/";
-import { Icon } from "@iconify/vue";
+<script lang="ts" setup>
+import { Btn } from './components/btn/'
+import { Icon } from '@iconify/vue'
+import { ref } from '@vue/reactivity'
 
-export default {
-  components: {
-    Btn,
-    Icon,
-  },
-};
+const showModal = ref(false)
+const toogleModal = () => {
+  showModal.value = !showModal.value
+}
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 2 ease;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(-50px);
+}
+
+.list-move, /* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+  position: absolute;
+}
+</style>
--- packages/core/src/components/btn/btn.ts
+++ packages/core/src/components/btn/btn.ts
@@ -1,79 +1,105 @@
-import {defineComponent, h, type PropType} from 'vue'
-import { Icon } from "@iconify/vue";
+import { defineComponent, h, type PropType } from 'vue'
+import { Icon } from '@iconify/vue'
 
-const baseClasses = 'px-[12px] py-[6px] rounded-[8px] text-white font-bold text-[14px] flex items-center justify-center gap-x-[8px]'
+const baseClasses =
+  'px-[12px] py-[6px] rounded-[8px] text-white font-bold text-[14px] flex items-center justify-center gap-x-[8px] hover:brightness-110 transition-all duration-200 active:brightness-90'
+const borderClasses =
+  'px-[10px] py-[6px] rounded-[8px] font-bold text-[14px] flex items-center justify-center gap-x-[8px] border-[2px]'
 
-
- type BtnType = 'primary' | 'secondary' | 'danger' | 'success' | 'warning' | 'info' | 'light' | 'dark' | 'link' | 'outline-primary' | 'outline-secondary' | 'outline-danger' | 'outline-success' | 'outline-warning' | 'outline-info' | 'outline-light' | 'outline-dark' | 'outline-link'
-
+type BtnType =
+  | 'primary'
+  | 'secondary'
+  | 'danger'
+  | 'success'
+  | 'warning'
+  | 'info'
+  | 'light'
+  | 'dark'
+  | 'link'
+  | 'outline-primary'
+  | 'outline-secondary'
+  | 'outline-danger'
+  | 'outline-success'
+  | 'outline-warning'
+  | 'outline-info'
+  | 'outline-light'
+  | 'outline-dark'
+  | 'outline-link'
 
 export const Btn = defineComponent({
-    name: 'Btn',
-    props: {
-        btnType: {
-            type: String as PropType<BtnType>,
-            default: 'primary'
-        },
-        text: {
-            type: String as PropType<string>,
-            required: true
-        },
-        bg: {
-            type: String as PropType<string>,
-            default: '#073368'
-        },
-        color: {
-            type: String as PropType<string>,
-            default: '#fff'
-        },
-        startIcon: {
-            type: String as PropType<string>,
-            default: ''
-        },
-        endIcon: {
-            type: String as PropType<string>,
-            default: ''
-        },
-            iconWidth: {
-                type: String as PropType<string>,
-                default: '24px'
-            },
-            iconColor: {
-                type: String as PropType<string>,
-                default: '#fff'
-            }
-
+  name: 'Btn',
+  props: {
+    btnType: {
+      type: String as PropType<BtnType>,
+      default: 'primary',
     },
-        
-    render() {
-        return h('button', {
-            class: baseClasses,
-            style: {
-                background: this.bg,
-                color: this.color
-            }
-        }, [
-            this.startIcon && h(Icon, {
-               
-                icon: this.startIcon,
-                width: this.iconWidth,
-                color: this.iconColor
-       
-                
-            }),
-            this.text,
-            this.endIcon && h(Icon, {
-                icon: this.endIcon,
-                width: this.iconWidth,
-                color: this.iconColor
+    text: {
+      type: String as PropType<string>,
+      required: true,
+    },
+    bg: {
+      type: String as PropType<string>,
+      default: '#073368',
+    },
+    color: {
+      type: String as PropType<string>,
+      default: '#fff',
+    },
+    startIcon: {
+      type: String as PropType<string>,
+      default: '',
+    },
+    endIcon: {
+      type: String as PropType<string>,
+      default: '',
+    },
+    iconWidth: {
+      type: String as PropType<string>,
+      default: '24px',
+    },
+    iconColor: {
+      type: String as PropType<string>,
+      default: '#fff',
+    },
+    border: {
+      type: Boolean as PropType<boolean>,
+      default: false,
+    },
+  },
 
-                })
-        ])
-
-
-        
-    }
-
-  
+  render() {
+    return h(
+      'button',
+      {
+        class: this.border ? borderClasses : baseClasses,
+        style: {
+          background: this.border ? 'transparent' : this.bg,
+          color: this.border ? this.bg : this.color,
+          borderColor: this.border ? this.bg : 'transparent',
+        },
+        // habdle click when button is clicked
+        onClick: () => {
+          console.log('clicked')
+        },
+      },
+      [
+        this.startIcon &&
+          h(Icon, {
+            icon: this.startIcon,
+            width: this.iconWidth,
+            color: this.border ? this.bg : this.iconColor,
+          }),
+        this.text,
+        this.endIcon &&
+          h(Icon, {
+            icon: this.endIcon,
+            width: this.iconWidth,
+            color: this.border ? this.bg : this.iconColor,
+          }),
+      ]
+    )
+  },
 })
+// emit click event
+
 export type Btn = InstanceType<typeof Btn>
