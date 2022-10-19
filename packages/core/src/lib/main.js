bd7cd2b3c2e1041a93c0605b42ced0e428952bca
abusiraj84 <abusiraj84@gmail.com>
2022-10-19 15:06:21 +0300

    fix(fix vite config): build issues

---
+++ packages/core/src/lib/main.js
@@ -0,0 +1,5 @@
+import { Btn } from "../components/btn/";
+import { Icon } from "@iconify/vue";
+import styles from "../index.css";
+
+export { Btn, Icon, styles };
--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,4 +1,7 @@
+/// <reference types="vitest" />
+
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,9 +9,45 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    transformMode: {
+      web: [/.[tj]sx$/],
+    },
+  },
+  build: {
+    // cssCodeSplit: true,
+    // sourcemap: true,
+
+    lib: {
+      // formats: ['umd', 'es', 'cjs'],
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, './src/lib/main.js'),
+      name: 'Lib',
+      fileName: (format) => `lib.${format}.js`,
+
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
   }
+
+
 })
