e963d16a5b56fabd203c22a648e1ca5d9774cc07
abusiraj84 <abusiraj84@gmail.com>
2022-10-20 15:55:58 +0300

    fix(card name): card comp

--- packages/core/src/lib/main.js
+++ packages/core/src/lib/main.js
@@ -2,4 +2,4 @@ import { Btn, Card } from '../components'
 import { Icon } from '@iconify/vue'
 import styles from '../index.css'
 
-export { Btn, Icon, card, styles }
+export { Btn, Icon, Card, styles }
